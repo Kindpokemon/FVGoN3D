2fd3d75b0d168e6422327760ab2557abc5360d7a
Kindpokemon <kindpokemon@gmail.com>
2016-05-11 23:50:57 -0500

    timing is everything

--- Assets/Scripts/WorldStuff/DayNight.cs
+++ Assets/Scripts/WorldStuff/DayNight.cs
@@ -3,60 +3,50 @@ using System.Collections;
 
 public class DayNight : MonoBehaviour {
 
-	public int dayLength;   //in minutes
-	public int dayStart;
-	public int nightStart;   //also in minutes
-	public int currentTime;
-	public float cycleSpeed;
-	public bool isDay;
-	public Vector3 sunPosition;
-	public Light sun;
-	public GameObject earth;
-	
-	// Day and Night Script for 2d,
-	// Unity needs one empty GameObject (earth) and one Light (sun)
-	// make the sun a child of the earth
-	// reset the earth position to 0,0,0 and move the sun to -200,0,0
-	// attach script to sun
-	// add sun and earth to script publics
-	// set sun to directional light and y angle to 90
-	
-	
-	void Start() {
-		dayLength = 1440;
-		dayStart = 300;
-		nightStart = 1200;
-		currentTime = 720;
-		StartCoroutine( TimeOfDay ());
-		earth = gameObject.transform.parent.gameObject;
+	public Transform Sun;
+	public float dayCycleInMinutes = 20;
+
+	public const float SECOND = 1;
+	public const float MINUTE = 60 * SECOND;
+	public const float HOUR = 60 * MINUTE;
+	public const float DAY = 24 * HOUR;
+	public const float MONTH = 30 * DAY;
+	public const float YEAR = 12 * MONTH;
+	private const float DEGREES_PER_SECOND = 360 / DAY;
+	private float _degreeRotation;
+	private float _timeofDay;
+
+	public static int currentSeconds = 0;
+	public static int currentMinutes = 0;
+	public static int currentHours = 0;
+	public static int currentDays = 0;
+	public static int currentMonths = 0;
+	public static int currentYears = 0;
+
+	float degreesRotated = 0;
+
+	// Use this for initialization
+	void Start () {
+		_timeofDay = 0;
+		_degreeRotation = DEGREES_PER_SECOND * DAY / (dayCycleInMinutes * MINUTE);
+		Time.timeScale = 1.0f;
 	}
-	
-	void Update() {
-		
-		if (currentTime > 0 && currentTime < dayStart) {
-			isDay =false;
-			sun.intensity = 0;
-		} else if (currentTime >= dayStart && currentTime < nightStart) {
-			isDay = true;
-			sun.intensity = 1;
-		} else if (currentTime >= nightStart && currentTime < dayLength) {
-			isDay = false;
-			sun.intensity = 0;
-		} else if (currentTime >= dayLength) {
-			currentTime = 0;
-		}
-		float currentTimeF = currentTime;
-		float dayLengthF = dayLength;
-		earth.transform.eulerAngles =  new Vector3 (0, 0, (-(currentTimeF / dayLengthF) * 360)+90);
-	}
-	
-	IEnumerator TimeOfDay(){
-		while (true) {
-			currentTime += 1;
-			int hours = Mathf.RoundToInt( currentTime / 60);
-			int minutes = currentTime % 60;
-			Debug.Log (hours+":"+minutes);
-			yield return new WaitForSeconds(1F/cycleSpeed);
+
+	// Update is called once per frame
+	void Update () {
+		if (degreesRotated < 360) {
+			Sun.Rotate (new Vector3 (_degreeRotation, 0, 0) * Time.deltaTime);
+			degreesRotated += _degreeRotation*Time.deltaTime;
+
+			_timeofDay += Time.deltaTime;
+			Debug.Log (_degreeRotation);
+			currentSeconds += (int)Mathf.Floor ((_degreeRotation*Time.deltaTime)*10.39f*24);
+			currentMinutes = currentSeconds / 60;
+			currentHours = currentMinutes / 60;
+			currentDays = currentHours / 24;
+			currentMonths = currentDays / 30;
+			currentYears = currentMonths / 12;
+			Debug.Log ("Seconds: " + currentSeconds + ", Minutes: " + currentMinutes + ", Hours: " + currentHours + ", Days: " + currentDays);
 		}
 	}
 }
